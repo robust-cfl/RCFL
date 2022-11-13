26590ae1be8360ec1cd660c54dee7c806c4ad0f4
robust-cfl <tdyegooogle@gmail.com>
2022-11-13 10:50:59 +0800

    fedavg

---
+++ algorithm/fedavg_run.py
@@ -0,0 +1,102 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '../')))
+
+import copy
+import torch
+import wandb
+import random
+import models.neural_nets as neural_nets
+import data.data_utils as data_utils
+from torch.utils.tensorboard import SummaryWriter
+from algorithm.fedavg_client import Client
+from algorithm.fedavg_server import Server
+from utils.args import parse_args
+from utils.utils import setup_seed
+from prettytable import PrettyTable
+from opacus.validators import ModuleValidator
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def run(hp):
+    print(hp)
+
+    # load the data and split it among clients
+    if hp.dataset == 'digit5':
+        train_loaders, test_loaders, public_loader, stats = data_utils.get_digit5(hp=hp, verbose=True)
+    elif hp.dataset == 'office':
+        train_loaders, test_loaders, public_loader, stats = data_utils.get_office(hp=hp, verbose=True)
+    elif hp.dataset == 'home':
+        train_loaders, test_loaders, public_loader, stats = data_utils.get_office_home(hp=hp, verbose=True)
+    else:
+        assert hp.dataset == 'pacs'
+        train_loaders, test_loaders, public_loader, stats = data_utils.get_pacs(hp=hp, verbose=True)
+    setup_seed(rs=hp.seed)
+    # instantiate clients and server with neural net
+    model = getattr(neural_nets, hp.model)()  # object
+    ################################
+    # domain-classifier requires-grad False
+    for (key, param) in model.named_parameters():
+        if key.startswith('domain'):
+            param.requires_grad = False
+    if hp.dp:
+        model = ModuleValidator.fix(model)
+        ModuleValidator.validate(model, strict=False)
+    else:
+        pass
+    # object, need deepcopy
+    clients = [Client(train_loader=train_loader, test_loader=test_loader, model=copy.deepcopy(model).to(device), hp=hp,
+                      id_num=i) for i, (train_loader, test_loader) in enumerate(zip(train_loaders, test_loaders))]
+
+    server = Server(model=copy.deepcopy(model).to(device), hp=hp, stats=stats)
+
+    # Print optimizer Specs
+    print_model(D=clients[0])
+
+    # Start Distributed Training Process
+    print("Start Distributed Training..\n")
+    for c_round in range(1, hp.communicationRounds + 1):
+        print(f"Round {c_round}")
+        # Update learning rate
+        # exp decay
+        updated_lr = hp.lr * hp.lrDecay ** (c_round // hp.decayStep)
+
+        # server does
+        server.broadcast(clients=clients)
+        # test on training set and test set
+        summary = server.test_on_test_data(clients=clients)
+        server.train(clients=clients, lr=updated_lr)
+        if hp.dp:
+            summary.update({'epsilon': sum(server.epsilons)})
+        server.aggregate()
+        if hp.wandb:
+            wandb.log(summary)
+        else:
+            for tag, value in summary.items():
+                writer.add_scalar(tag, value, c_round)
+
+
+def print_model(D):
+    print("Model {}:".format(D.hp.model))
+    n = 0
+    for key, value in D.model.named_parameters():
+        print(' -', '{:30}'.format(key), list(value.shape))
+        n += value.numel()
+    print("Total number of Parameters: ", n)
+    print()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    if args.wandb:
+        wandb.init(entity='tdye24', project='RobustCFL')
+        wandb.watch_called = False
+        config = wandb.config
+        config.update(args)
+    else:
+        writer = SummaryWriter(f'./runs/{args.logname}')
+        args.writer = writer
+        config = args
+    run(hp=config)
---
+++ algorithm/fedavg_server.py
@@ -0,0 +1,142 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import numpy as np
+from utils.utils import *
+from copy import deepcopy
+from data.data_utils import *
+from sklearn import preprocessing
+from sklearn.cluster import KMeans
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+class Server:
+    def __init__(self, model, hp, stats):
+        # models
+        self.model = model
+        # Hyper-parameters
+        self.hp = hp
+        # Loss function
+        self.loss_fn = nn.CrossEntropyLoss()
+
+        self.client_sizes = torch.Tensor(stats["split"]).cuda()
+
+        self.updates = None
+        self.epsilons = None
+
+    def broadcast(self, clients):
+        for client in clients:
+            client.update(deepcopy(self.model.state_dict()))
+
+    def train(self, clients, lr):
+        updates = []
+        epsilons = []
+        for client in clients:
+            print(client.id)
+            if self.hp.dp:
+                theta, epsilon = client.classify_training(lr=lr)
+                epsilons.append(epsilon)
+            else:
+                theta = client.classify_training(lr=lr)
+            updates += [(self.client_sizes[client.id], theta)]
+        self.updates = updates
+        if self.hp.dp:
+            self.epsilons = epsilons
+
+    def aggregate(self):
+        updates = self.updates
+        tmp_theta = deepcopy(self.model.state_dict())
+        total_samples = 0
+
+        for (size, theta) in updates:
+            if total_samples == 0:
+                # first client
+                for k in tmp_theta.keys():
+                    if self.hp.dp:
+                        t_k = '_module.' + k
+                        tmp_theta[k] = size * theta[t_k]  # 去掉——module.
+                    else:
+                        tmp_theta[k] = size * theta[k]
+            else:
+                # later client
+                for k in tmp_theta.keys():
+                    if self.hp.dp:
+                        t_k = '_module.' + k
+                        tmp_theta[k] += size * theta[t_k]
+                    else:
+                        tmp_theta[k] += size * theta[k]
+
+            total_samples += size
+
+        for k in tmp_theta.keys():
+            tmp_theta[k] /= total_samples
+
+        # update server models
+        self.model.load_state_dict(tmp_theta)
+
+    def test_on_test_data(self, clients):
+        test_loss_list, test_acc_list,  = [], []
+        for client in clients:
+            results_test = client.validation(loader=client.test_loader)
+            test_loss_list += [results_test['loss']]
+            test_acc_list += [results_test['accuracy']]
+        clients_samples_num = [self.client_sizes[client.id] for client in clients]
+        average_test_loss = sum(
+            [clients_samples_num[i] * test_loss_list[i] for i in range(len(clients_samples_num))]) / sum(
+            clients_samples_num)
+        average_test_acc = sum(
+            [clients_samples_num[i] * test_acc_list[i] for i in range(len(clients_samples_num))]) / sum(
+            clients_samples_num)
+
+        print(f"Standalone performance--test loss: {average_test_loss}, test acc: {average_test_acc}")
+
+        summary = {
+            'test loss': average_test_loss,
+            'test acc': average_test_acc
+        }
+        return summary
+
+    def test(self, clients):
+        training_loss_list, test_loss_list = [], []
+        training_acc_list, test_acc_list = [], []
+        for client in clients:
+            results_train = client.validation(loader=client.train_loader)
+            results_test = client.validation(loader=client.test_loader)
+            training_loss_list += [results_train['loss']]
+            training_acc_list += [results_train['accuracy']]
+
+            test_loss_list += [results_test['loss']]
+            test_acc_list += [results_test['accuracy']]
+        clients_samples_num = [self.client_sizes[client.id] for client in clients]
+        average_training_loss = sum(
+            [clients_samples_num[i] * training_loss_list[i] for i in range(len(clients_samples_num))]) / sum(
+            clients_samples_num)
+        average_training_acc = sum(
+            [clients_samples_num[i] * training_acc_list[i] for i in range(len(clients_samples_num))]) / sum(
+            clients_samples_num)
+        average_test_loss = sum(
+            [clients_samples_num[i] * test_loss_list[i] for i in range(len(clients_samples_num))]) / sum(
+            clients_samples_num)
+        average_test_acc = sum(
+            [clients_samples_num[i] * test_acc_list[i] for i in range(len(clients_samples_num))]) / sum(
+            clients_samples_num)
+
+        print(
+            f"One-shot performance, training loss: {average_training_loss}, training acc: {average_training_acc}")
+        print(f"One-shot performance, test loss: {average_test_loss}, test acc: {average_test_acc}")
+
+        summary = {
+            'training loss': average_training_loss,
+            'training acc': average_training_acc,
+            'test loss': average_test_loss,
+            'test acc': average_test_acc
+        }
+        return summary
+
+    @staticmethod
+    def determine_cluster_index_using_kmeans(X, n_cluster=4):
+        estimator = KMeans(n_clusters=n_cluster)
+        estimator.fit(X)
+        labels_pred = estimator.labels_
+        return labels_pred
